ae051ae07a008a0587713e1cdba9c2a3c57b774b
Shub4am <shub4am@gmail.com>
2023-09-22 01:08:24 +0530

    scrollable list

--- components/MoviesList.tsx
+++ components/MoviesList.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useMoviesContext } from '@/providers/MoviesProvider';
 import Image from 'next/image';
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
@@ -10,6 +10,19 @@ interface MoviesListProps {
 
 const MoviesList: React.FC<MoviesListProps> = ({ title }) => {
   const { movieData } = useMoviesContext();
+
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  const handleScrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 300;
+    }
+  };
+  const handleScrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 300;
+    }
+  };
 
   const MovieListCard = () => {
     return movieData?.map((movie) => (
@@ -42,20 +55,29 @@ const MoviesList: React.FC<MoviesListProps> = ({ title }) => {
   };
 
   return (
-    <div className="pl-28 mt-2 py-2 mb-10">
+    <div className="pl-28 pr-5 mt-2 py-2 mb-10">
       <div className="font-semibold text-xl">
         <p className="pb-2">{title}</p>
-        <div className="flex overflow-x-hidden hover:overflow-x-scroll ">
-          <div className="flex w-fit items-center gap-x-2">
-            <AiOutlineLeft size={40} className="absolute text-blue-700 " />
+        <div className="flex overflow-x-hidden relative items-center ">
+          <AiOutlineLeft
+            size={40}
+            className="absolute text-white cursor-pointer left-3 hover:backdrop-blur-md hover:rounded-full"
+            onClick={handleScrollLeft}
+          />
+          <div
+            className="flex space-x-2 overflow-x-hidden scroll-smooth "
+            ref={scrollRef}
+          >
             <MovieListCard />
             <MovieListCard />
             <MovieListCard />
-            <AiOutlineRight
-              size={40}
-              className="absolute text-blue-700 right-1"
-            />
+            <MovieListCard />
           </div>
+          <AiOutlineRight
+            size={40}
+            className="absolute text-white right-1 cursor-pointer hover:backdrop-blur-md hover:rounded-full"
+            onClick={handleScrollRight}
+          />
         </div>
       </div>
     </div>
