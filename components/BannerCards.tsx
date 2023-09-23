760710b5a9b328e694162a0dfaad57c5f3a870a9
Shub4am <shub4am@gmail.com>
2023-09-23 17:33:17 +0530

    show first 4 movies

--- components/BannerCards.tsx
+++ components/BannerCards.tsx
@@ -12,7 +12,7 @@ const BannerCards: React.FC<BannerCardsProps> = ({ movies }) => {
   return (
     <div className="absolute right-0 md:right-10 top-[60%] lg:top-[48%] mt-4">
       <div className="flex flex-row gap-2  cursor-pointer ">
-        {movies.map((movie) => (
+        {movies.slice(0, 4).map((movie) => (
           <div
             key={movie.id}
             className="group  bg-zinc-800 relative w-14 h-10 md:w-[90px] md:h-[51px] rounded-lg"
