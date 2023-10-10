0461cc097dd1c9c0c3d6c2ae06c4941dd65dec96
Shub4am <shub4am@gmail.com>
2023-10-10 10:12:12 +0530

    card component added

---
+++ components/Cards.tsx
@@ -0,0 +1,165 @@
+'use client';
+import React, { useState, FC } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { Movie } from '@/types';
+import { useMoviesContext } from '@/providers/MoviesProvider';
+
+import { AiOutlinePlus } from 'react-icons/ai';
+import { BsFillPlayFill } from 'react-icons/bs';
+
+interface CardsProps {
+  title: string;
+}
+
+const Cards: FC<CardsProps> = ({ title }) => {
+  const { movieData } = useMoviesContext();
+  const [hoveredMovie, setHoveredMovie] = useState<Movie | null>(null);
+
+  const router = useRouter();
+
+  const handleMouseEnter = (movie: Movie) => {
+    setHoveredMovie(movie);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredMovie(null);
+  };
+
+  const handleBtnClick = (movie: Movie) => {
+    router.push(`/${movie.id}`);
+  };
+
+  return (
+    <div className="pl-28 pr-5 py-2 mb-10 select-none">
+      <p className="font-semibold text-xl">{title}</p>
+      <div className="relative flex gap-x-2">
+        {movieData?.map((movie) => (
+          <div
+            key={movie.id}
+            className="
+              flex 
+              relative 
+              hover:scale-[1.7]
+              hover:-translate-y-6
+              hover:z-20
+              hover:opacity-100
+              transition
+            "
+            onMouseEnter={() => handleMouseEnter(movie)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Image
+              loading="lazy"
+              unoptimized
+              src={movie.thumbnail_url || ''}
+              alt={movie.title || 'Movie Title'}
+              width={90}
+              height={120}
+              className="
+                rounded-lg
+                object-cover
+                w-[90px]
+                h-[120px]
+                md:w-[150px]
+                md:h-[200px]
+                brightness-[.8]
+              "
+            />
+
+            {/* Show card when movie is hovered */}
+            {hoveredMovie === movie && (
+              <div
+                className="
+                  absolute 
+                  inset-0 
+                  z-40 
+                  g-transparent 
+                  text-center 
+                  flex 
+                  flex-col 
+                  items-center 
+                  justify-end 
+                  bg-opacity-25 
+                  rounded-lg 
+                  duration-300
+                  shadow-black
+                  shadow-xl
+                "
+              >
+                <div
+                  className="
+                    flex 
+                    flex-col 
+                    w-full 
+                    h-3/5 
+                    bg-hoverBg 
+                    rounded-b-md
+                    overflow-hidden
+                  "
+                >
+                  <div
+                    className="
+                      w-full
+                      p-1
+                      flex
+                      items-center
+                      gap-x-1
+                    "
+                  >
+                    <button
+                      onClick={() => handleBtnClick(movie)}
+                      className="
+                      w-4/5
+                      p-1
+                      bg-slate-200 
+                      self-center
+                      text-zinc-900
+                      font-medium
+                      text-sm
+                      rounded-md
+                      flex
+                      items-center
+                      justify-center
+                      hover:shadow-lg
+                      hover:shadow-blue-950
+                    "
+                    >
+                      <BsFillPlayFill />
+                      Watch Now
+                    </button>
+                    <span
+                      className="
+                        bg-gray-700
+                        rounded-md
+                        p-[5px]
+                        hover:scale-105
+                      "
+                    >
+                      <AiOutlinePlus />
+                    </span>
+                  </div>
+
+                  {/* year genre duration */}
+                  <div className="text-[8px] truncate p-1 ">
+                    {movie.year} • {movie.duration} • English • {movie.genre}
+                  </div>
+                  {/* description */}
+                  <p
+                    className="text-[8px] px-1 text-clip break-normal line-clamp-4
+                  "
+                  >
+                    {movie.description}
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
