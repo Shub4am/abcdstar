d223b4ed7db437fb42057f184247093388821369
Shub4am <shub4am@gmail.com>
2023-10-12 18:33:52 +0530

    layout file for movies route

--- app/(movies)/[movieId]/layout.tsx
+++ app/(movies)/[movieId]/layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../../../app/globals.css';
+import { MoviesProvider } from '@/providers/MoviesProvider';
 
 export default function MoviesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full">
+      <MoviesProvider>{children}</MoviesProvider>
+    </div>
+  );
 }
