70b589927aff012c16d5048776d5c8715e71e636
Shub4am <shub4am@gmail.com>
2023-10-14 10:53:12 +0530

    update movieId page

--- app/(movies)/[movieId]/page.tsx
+++ app/(movies)/[movieId]/page.tsx
@@ -8,6 +8,7 @@ import { BiArrowBack } from 'react-icons/bi';
 import { BsPlayBtn } from 'react-icons/bs';
 import { HiOutlineChatBubbleBottomCenterText } from 'react-icons/hi2';
 import { useRoutes } from '@/hooks/useRoutes';
+import Error from './error';
 
 interface MovieIdPageProps {
   params: {
@@ -41,28 +42,30 @@ const WatchMovie = ({ params }: MovieIdPageProps) => {
       onContextMenu={handleContextMenu}
       className="text-white bg-midnight h-screen w-screen flex flex-col items-center justify-center"
     >
-      <nav className="fixed top-0 w-full p-4 z-40 flex items-center gap-2 bg-midnight bg-opacity-40 drop-shadow-2xl shadow-white">
-        <BiArrowBack
-          size={30}
-          onClick={handleBackBtn}
-          className="w-10 text-white cursor-pointer transition ease-in"
-        />
-        <p className="font-semibold">{matchedMovie && matchedMovie.title}</p>
-        <div className="fixed hidden sm:flex gap-x-8 items-center right-4 px-4">
-          <div className="flex items-center gap-x-3 hover:scale-110 cursor-pointer">
-            <BsPlayBtn size={22} />
-            <p className="">Quality</p>
-            <p className="text-gray-400">Full HD</p>
-          </div>
-          <div className="flex  items-center gap-x-3 hover:scale-110 cursor-pointer">
-            <HiOutlineChatBubbleBottomCenterText size={22} />
-            <p>Audio Language</p>
-          </div>
-        </div>
-      </nav>
-
       {matchedMovie && (
         <>
+          <nav className="fixed top-0 w-full p-4 z-40 flex items-center gap-2 bg-midnight bg-opacity-40 drop-shadow-2xl shadow-white">
+            <BiArrowBack
+              size={30}
+              onClick={handleBackBtn}
+              className="w-10 text-white cursor-pointer transition ease-in"
+            />
+            <p className="font-semibold">
+              {matchedMovie && matchedMovie.title}
+            </p>
+            <div className="fixed hidden sm:flex gap-x-8 items-center right-4 px-4">
+              <div className="flex items-center gap-x-3 hover:scale-110 cursor-pointer">
+                <BsPlayBtn size={22} />
+                <p className="">Quality</p>
+                <p className="text-gray-400">Full HD</p>
+              </div>
+              <div className="flex  items-center gap-x-3 hover:scale-110 cursor-pointer">
+                <HiOutlineChatBubbleBottomCenterText size={22} />
+                <p>Audio Language</p>
+              </div>
+            </div>
+          </nav>
+
           <p>{matchedMovie && matchedMovie.title}</p>
           <video
             className=" h-screen w-full object-cover absolute"
