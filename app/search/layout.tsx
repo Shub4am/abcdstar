42b2d87bb3eb7f1ede604fd136c9bf9018e74c50
Shub4am <shub4am@gmail.com>
2023-10-19 11:22:53 +0530

    search page

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import '../../app/globals.css';
+import Sidebar from '@/components/Sidebar';
+import Cards from '@/components/Cards';
+import { MoviesProvider } from '@/providers/MoviesProvider';
+import Footer from '@/components/Footer';
 
 export default function SearchLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-screen w-full">
+      <MoviesProvider>
+        <Sidebar>{}</Sidebar>
+        {children}
+        <Cards title="Popular Searches" />
+        <Footer />
+      </MoviesProvider>
+    </div>
+  );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
+
+import { BiSearch } from 'react-icons/bi';
+import { RxCross2 } from 'react-icons/rx';
+
 export default function Search() {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [clearIconVisible, setClearIconVisible] = useState(false);
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    setClearIconVisible(!!e.target.value);
+  };
+  const handleClearInput = () => {
+    setInputValue('');
+    setClearIconVisible(false);
+  };
+
   return (
-    <div className="text-white">
-      <p>Search</p>
+    <div className="relative h-fit w-full mt-8 flex justify-center pl-12 pb-10">
+      <form className="w-[90%] h-[70px] flex items-center justify-start px-4 bg-[#252833] outline-none placeholder-gray-500 capitalize rounded-md text-gray-400 focus-within:text-white">
+        <BiSearch size={28} className="absolute inset-y-0 top-6 " />
+        <input
+          type="text"
+          placeholder="Movies, shows and more"
+          className=" w-full h-full px-10 bg-[#252833] outline-none placeholder-gray-400 focus:text-white font-medium text-xl rounded-md"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        {clearIconVisible && (
+          <RxCross2
+            size={28}
+            className="cursor-pointer"
+            onClick={handleClearInput}
+          />
+        )}
+      </form>
     </div>
   );
 }
