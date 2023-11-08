ed51b87e6d461bd31e6e355e6774e505c1de0b46
Shub4am <shub4am@gmail.com>
2023-11-09 01:33:16 +0530

    Fix persistSession warning

--- config/supabaseClient.ts
+++ config/supabaseClient.ts
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  auth: { persistSession: false },
+});
 
 export default supabase;
