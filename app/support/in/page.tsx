5db9a7affaaa18b32e888cc882879e3a4ab41571
Shub4am <shub4am@gmail.com>
2023-11-14 08:04:39 +0530

    Add support faq

---
+++ app/support/in/page.tsx
@@ -0,0 +1,74 @@
+export default function Page() {
+  return (
+    <div className="text-white flex flex-col justify-center  py-4 gap-y-6 px-32">
+      <h1 className="font-bold text-lg">Frequently Asked Questions</h1>
+      <h2 className="font-semibold text-lg">1. What is ABCD+?</h2>
+      <p>
+        ABCD+ is a video streaming app that offers a diverse range of content,
+        including movies, TV shows, and exclusive originals. It provides users
+        with a personalized and immersive streaming experience.
+      </p>
+
+      <h2 className="font-semibold text-lg">
+        2.Is ABCD+ available on multiple devices?
+      </h2>
+      <p>
+        Yes, ABCD+ is available on various devices, including smartphones,
+        tablets, smart TVs, and web browsers. You can enjoy your favorite
+        content on the go or in the comfort of your home.
+      </p>
+
+      <h2 className="font-semibold text-lg">
+        3. Can I watch content offline on ABCD+?
+      </h2>
+      <p>
+        Yes, ABCD+ allows you to download select movies and TV shows for offline
+        viewing. This feature is available on our mobile apps..
+      </p>
+
+      <h2 className="font-semibold text-lg">4. How can I reset my password?</h2>
+      <p>
+        If you need to reset your password, go to the login screen and click on
+        the &quot;Forgot Password&quot; link. Follow the instructions sent to
+        your registered email address to reset your password.
+      </p>
+
+      <h2 className="font-semibold text-lg">
+        5. What types of content does ABCD+ offer?
+      </h2>
+      <p>
+        ABCD+ provides a wide range of content, including blockbuster movies,
+        popular TV series, documentaries, and exclusive originals across various
+        genres.
+      </p>
+
+      <h2 className="font-semibold text-lg">
+        6. How does ABCD+ recommend content to me?
+      </h2>
+      <p>
+        ABCD+ uses algorithms to analyze your viewing history and preferences,
+        providing personalized recommendations based on your interests.
+      </p>
+
+      <h2 className="font-semibold text-lg">
+        7. Can I share my ABCD+ account with others?
+      </h2>
+      <p>
+        Your ABCD+ account is for personal use only, and sharing account
+        credentials with others is against our terms of service. Each account
+        should be used by the account holder.
+      </p>
+
+      <h2 className="font-semibold text-lg">
+        {' '}
+        8. Is there a free trial period for ABCD+?
+      </h2>
+      <p>
+        Yes, ABCD+ offers a free trial period for new users. During this period,
+        you can explore the app and enjoy its features before deciding to
+        subscribe. If you have additional questions, please feel free to contact
+        us at hello@notarealemail.com. Thank you for choosing ABCD+!
+      </p>
+    </div>
+  );
+}
